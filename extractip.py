47a0c49a23aab5725dbfd11bc771383ce2388d15
Daniel Facuri <danieus_@hotmail.com>
2017-10-27 21:15:50 -0200

    Update extractip.py

--- extractip.py
+++ extractip.py
@@ -4,10 +4,7 @@ import subprocess
 import os
 import netifaces
 netifaces.interfaces()
-#netifaces.ifaddresses('choose some interface like eth1-2 1 per')
-#for interface in netifaces.interfaces():
-    #print netifaces.ifaddresses(interface)[netifaces.AF_INET]
 for interface in netifaces.interfaces():
 	for link in netifaces.ifaddresses(interface)[netifaces.AF_INET]:
-					#cmd=shlex.split("ping -c1 ['addr']")
+	 #try to ping ip on file #cmd=shlex.split("ping -c1 ['addr']")
      		    	print link['addr']
