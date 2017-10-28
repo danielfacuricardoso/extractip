2b79ed2f08a941dfa1432f72a765ec0ccb7bdf0a
Daniel Facuri <danieus_@hotmail.com>
2017-10-27 22:52:09 -0200

    Update extractip.py

--- extractip.py
+++ extractip.py
@@ -7,4 +7,8 @@ netifaces.interfaces()
 for interface in netifaces.interfaces():
 	for link in netifaces.ifaddresses(interface)[netifaces.AF_INET]:
 	 #try to ping ip on file #cmd=shlex.split("ping -c1 ['addr']")
-     		    	print link['addr']
+     		       with open('test1','w') as f:
+#                       sys.stdout=f
+                        print link['addr']
+   # problem:neet to write 4 lines but it just write last, its mean that it's overwrite all lines before last.
+		
