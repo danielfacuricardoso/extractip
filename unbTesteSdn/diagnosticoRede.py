0307aa2a9de035f2067caa266f33d21fb58c87bd
Antonio Santos <antoniosabsb@gmail.com>
2017-11-26 22:18:47 -0200

    refactory do script

--- unbTesteSdn/diagnosticoRede.py
+++ unbTesteSdn/diagnosticoRede.py
@@ -19,11 +19,11 @@ for linha in listaComandosSSH:
         continue
     print 'comando: '+linha
     comandoAtual = linha.split(',')
+    buff = ''
     try:
         if(len(comandoAtual) == 1):
             param1 = str(comandoAtual[0])
-            buff = ''
-            # EXECUTA COMANDO ls -la E AGUARDA 'root@sdn-r1-borda1:~# '
+            # EXECUTA COMANDO ping e AGUARDA ':~# ' OU TIMEOUT
             canal.send(param1)
             buff = ''
             while not buff.endswith(':~# '):
@@ -32,27 +32,24 @@ for linha in listaComandosSSH:
             # EXIBE O CONTEUDO DA VARIAVEL 'buff'
             if(linha.startswith('ping')):
                 buff=int(re.findall(r'(\d+)%',buff)[0])
-            print 'buff', buff
         if(len(comandoAtual) == 2):
             param1 = str(comandoAtual[0])
             param2 = str(comandoAtual[1])
-
-            #SSH E HOST E AGUARDA A SENHA COM MSG 's password:'
+            #SSH E HOST E AGUARDA A SENHA COM MSG 's password:'  OU TIMEOUT
             canal.send(param1+'\n')
             buff = ''
             while not buff.endswith('\'s password: '):
                 resp = canal.recv(9999)
                 buff += resp
-            print (buff)
-            #ENVIA A SENHA E AGUARDA
+            #ENVIA A SENHA E AGUARDA  OU TIMEOUT
             canal.send(param2)
             buff = ''
             while not buff.endswith(':~# '):
                 resp = canal.recv(9999)
                 buff += resp
-            print (buff)
     except (IOError,TypeError) as erro:
         print(erro)
-
+    if(not linha.startswith('ssh')):
+        print 'buff', buff
 listaComandosSSH.close()
 ssh.close()
