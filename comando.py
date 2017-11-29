cc00eba49f7b4808639ae5ee4999bb77757e36a0
Daniel Facuri <danieus_@hotmail.com>
2017-11-29 09:27:13 -0200

    Update comando.py

--- comando.py
+++ comando.py
@@ -1,31 +1,47 @@
-#!/usr/bin/env python
-#coding: utf-8
+#!/bin/usr/env  python
+#!coding: utf-8
 import paramiko
 import os
 import subprocess
 import sys
-l=[['r1','r2',0],['r2','r4',1]]
+import re
+#Autenticação R1
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect('192.168.3.2', username='root', password='admin')
-stdin, stdout, stderr = ssh.exec_command('ping -c 1 192.168.0.2 -q\n')  # pode ser redirecionado ou guardado em um lista
-a= stdout.read()
-estadoseg1=int(a.split('\n')[3].split(',')[2].split(" ")[1][:-1])
+ssh.connect('192.168.3.2')
+#Fim da Autenticação R1
+#Iniciando criação da Topologia - Roteador 1
+Topologia_Roteador_R1_Padrao = ['192.168.0.2','192.168.0.14','192.168.0.18','192.168.3.1']
+def probe_ok(ip):
+    stdin, stdout, stderr = ssh.exec_command('ping -c 1 ' + ip + ' -q')
+    segmento= stdout.read()
+    if (len(segmento)==0):
+       segmento='100'
+    else:
+#    print segmento
+        Segmento_Teste_Interface_Remota=int(re.findall(r'(\d+)%',segmento)[0])
+#    print Segmento_Teste_Interface_Remota
+    return Segmento_Teste_Interface_Remota != 100
+#TOPOLOGIA R1
+TopologiaUpR1 = [ip for ip in Topologia_Roteador_R1_Padrao if probe_ok(ip)]
+#print TopologiaUpR1
+#TOPOLOGIA R2
 
-#seg1ip = "192.168.0.2"
-#estadoseg1 = os.system("ping %s -c 2" % seg1ip)
-#estadoseg1 = os.system("ip addr show")
-#l=[['r1','r2',0],['r1','r2',1]]
-if estadoseg1 is not 0:
-    l[0][2]=1
-else:
-    l[0][2]=0
-if l[0][2]  is not 0:
-    print "segmento1,down"
-else:
-    print "segmento1,up"
+stdin, stdout, stderr = ssh.exec_command('python /root/comand7R2.py')
+TOPOLOGIA_R2=stdout.read().rstrip('\n').replace('[','').replace('"','').replace(' ','').replace(']','').replace('\'','').split(",")
+#print TOPOLOGIA_R2
+#print type(TOPOLOGIA_R2)
+#TOPOLOGIA R3
+stdin, stdout, stderr = ssh.exec_command('python /root/comand7R3.py')
+TOPOLOGIA_R3= stdout.read().rstrip('\n').replace('[','').replace('"','').replace(' ','').replace(']','').replace('\'','').split(",")
+#TOPOLOGIA R4
+stdin, stdout, stderr = ssh.exec_command('python /root/comand7R4.py')
+TOPOLOGIA_R4= stdout.read().rstrip('\n').replace('[','').replace('"','').replace(' ','').replace(']','').replace('\'','').split(",")
+## TOPOLOGIAGERAL ##
+TOPOLOGIAGERAL=[TopologiaUpR1,TOPOLOGIA_R2,TOPOLOGIA_R3,TOPOLOGIA_R4]
+print TOPOLOGIAGERAL
+#print type(TOPOLOGIAGERAL[1])
+#print TOPOLOGIAGERAL[1]
 
- # print("segmento 1 down")
-#else:
- #   print("segmento 1 up")
-#print stdout.readlines() #vê a saída do comando
+
+
