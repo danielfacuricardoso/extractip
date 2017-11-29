860ef33c749cb0e6d186f4bd94d90d9efe3d3205
antoniosabsb <antoniosabsb@gmail.com>
2017-11-29 15:41:40 -0200

    alterando os HOSTNAME por IPs(ficticios) e montando a listaTopologia

--- unbTesteSdn/networkxTeste.py
+++ unbTesteSdn/networkxTeste.py
@@ -1,19 +1,42 @@
 import networkx as nx
-import numpy as np
+"""import numpy as np
 import matplotlib.pyplot as plt
 import pylab
+"""
+TOPOLOGIAGERAL = [
+                    ['192.168.0.1','192.168.1.1'],
+                    ['192.168.1.1', '192.168.1.2', '192.168.1.3', '192.168.1.4'],
+                    ['192.168.1.2','192.168.1.4', '192.168.1.5'],
+                    ['192.168.1.3','192.168.1.4', '192.168.1.6'],
+                    ['192.168.1.4','192.168.1.1', '192.168.1.2', '192.168.1.3']
+                 ]
 
 ### 1 - INFORMAR A TOPOLOGIA ATIVA
 listaTopologia = []
-listaTopologia.append("CTR,R1, 9")
+peso = 2
+for topologiaEspecifica in TOPOLOGIAGERAL:
+    maquinaOrigem = 0
+    maquinaDestino = 1
+    for maquina in topologiaEspecifica:
+        if(maquina == topologiaEspecifica[maquinaOrigem]):
+            continue
+        else:
+            texto = topologiaEspecifica[maquinaOrigem]+","+maquina+","+str(peso)
+        listaTopologia.append(texto)
+    peso +=2
+
+"""listaTopologia.append("CTR,R1, 9")
 listaTopologia.append("R1, R2, 2")
 listaTopologia.append("R1, R3, 4")
 listaTopologia.append("R1, R4, 6")
-#listaTopologia.append("R2, R4, 2")
+listaTopologia.append("R2, R4, 2")
 listaTopologia.append("R2, H1, 9")
 listaTopologia.append("R3, R4, 3")
 listaTopologia.append("R3, H2, 9")
-
+listaTopologia.append("R4, R1, 6")
+listaTopologia.append("R4, R2, 2")
+listaTopologia.append("R4, R3, 3")
+"""
 G = nx.DiGraph()
 
 ### 2 - CARREGAR O OBJETO DO NETWORKX
@@ -26,7 +49,7 @@ for linha in listaTopologia:
     G.add_edges_from([(destino,origem)], weight=peso)
 
 ### 3 - DEFINE ESTILO PARA O CASO DE EXIBIR TOPOLOGIA GRAFICAMENTE
-val_map = {'CTR': 1.0, 'H1': 0.5714285714285714, 'H2': 0.0}
+"""val_map = {'CTR': 1.0, 'H1': 0.5714285714285714, 'H2': 0.0}
 values = [val_map.get(node, 0.45) for node in G.nodes()]
 edge_labels=dict([((u,v,),d['weight'])
                  for u,v,d in G.edges(data=True)])
@@ -35,9 +58,11 @@ edge_colors = ['blue' if not edge in red_edges else 'red' for edge in G.edges()]
 pos=nx.spring_layout(G)
 nx.draw_networkx_edge_labels(G,pos,edge_labels=edge_labels)
 nx.draw(G,pos, node_color = values, node_size=2500,edge_color=edge_colors,edge_cmap=plt.cm.Reds, with_labels=True)
-
+"""
 ### 4 - EXIBE MENOR CAMINHO
-print(nx.dijkstra_path(G, 'H1', 'H2', weight='weight'))
+#print(nx.dijkstra_path(G, 'H1', 'H2', weight='weight'))
+print(nx.dijkstra_path(G, '192.168.1.4', '192.168.1.6', weight='weight'))
+
 
 ### 5 - EXIBE TOPOLOGIA GRAFICAMENTE
-pylab.show()
+"""pylab.show()"""
